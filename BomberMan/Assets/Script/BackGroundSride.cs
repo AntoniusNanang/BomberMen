6d05183d2dc611c32647445f8c9b95cd5202eb9e
hyuga <kurihara.73091@gmail.com>
2019-10-02 11:21:25 +0900

    タイトルシーンの背景

---
+++ BomberMan/Assets/Script/BackGroundSride.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGroundSride : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.Translate(-0.5f, 0, 0);
+        if (transform.position.x < 100f)
+        {
+            transform.position = new Vector3(700f, 137f, 0);
+        }
+        // transform.position += Vector3.left * speed * Time.deltaTime;
+    }
+    // void OnBecameInvisible()
+    //{
+    //    float width = GetComponent<SpriteRenderer>().bounds.size.x;
+    //    transform.position += Vector3.right * width * spriteCount;
+    //}
+}
---
+++ BomberMan/Assets/Script/GradientTest.cs
@@ -0,0 +1,31 @@
+﻿namespace UnityEngine.UI
+{
+    [AddComponentMenu("UI/Effects/Custom/Gradient", 100)]
+    public class GradientTest : BaseMeshEffect
+    {
+        public Color32 Color1 = Color.yellow;
+        public Color32 Color2 = Color.red;
+        public override void ModifyMesh(VertexHelper vh)
+        {
+            if (!IsActive()) return;
+            UIVertex v = new UIVertex();
+            int idx = 0;
+            for (int i = 0; i < vh.currentVertCount; i++)
+            {
+                vh.PopulateUIVertex(ref v, i);
+                //switch (idx)
+                //{
+                //    case 0: v.color = new Color(0,0,0,0.2f); break;
+                //    case 1: v.color = Color.black; break;
+                //    //case 2: v.color = Color.black; break;
+                //}
+                //縦のグラデーション
+                v.color = (idx == 0 || idx == 3) ? Color1 : Color2;
+                //横のグラデーション
+                v.color = (idx == 0 || idx == 1) ? Color1 : Color2;
+                if (++idx >= 4) { idx = 0; }
+                vh.SetUIVertex(v, i);
+            }
+        }
+    }
+}
